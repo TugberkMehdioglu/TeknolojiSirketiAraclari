bebadcd053a5ad52a3ea7d831d4ddc8b667f7d3d
Tuğberk <84016118+TugberkMehdioglu@users.noreply.github.com>
2021-06-18 21:52:04 +0300

    Add project files.

---
+++ DepoAPI/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using DepoAPI.DesignPatterns.SingletonPattern;
+using DepoAPI.DTOClasses;
+using DepoAPI.Models.Context;
+using DepoAPI.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DepoAPI.Controllers
+{
+    public class HomeController : ApiController
+    {
+        MyContext _db;
+        public HomeController()
+        {
+            _db = DBTool.DBInstance;
+        }
+
+        [HttpPost]
+        public IHttpActionResult StockControl(StockDTO item)
+        {
+            if(item.ID > 0 && item.ProductName != null && item.UnitInStock > 0 && item.UnitPrice > 0)
+            {
+                Storage product = new Storage
+                {
+                    ID = item.ID,
+                    ProductName = item.ProductName,
+                    UnitInStock = item.UnitInStock,
+                    UnitPrice = item.UnitPrice
+                };
+
+                _db.Storages.Add(product);
+                _db.SaveChanges();
+
+                return Ok();
+            }
+            return BadRequest("Mandatory fields cannot be null");
+        }
+    }
+}
