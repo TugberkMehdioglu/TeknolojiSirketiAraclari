471012ba52b3a6f3cbdef6a911d45d19817a9029
Tuğberk <84016118+TugberkMehdioglu@users.noreply.github.com>
2021-08-06 17:25:35 +0300

    DukyanWinUI eklendi

---
+++ DukyanWinUI/DukyanWinUI/Form1.cs
@@ -0,0 +1,75 @@
+﻿using DukyanWinUI.DTOClasses;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DukyanWinUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44339/api/");
+                Task<HttpResponseMessage> getTask = client.GetAsync("home/getstock");
+
+                HttpResponseMessage result = new HttpResponseMessage();
+
+                try
+                {
+                    result = getTask.Result;
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("DepoAPI bağlantıyı reddetti");
+                    return;
+                }
+
+                if (result.IsSuccessStatusCode)
+                {
+                    string contentString = await getTask.Result.Content.ReadAsStringAsync();
+
+                    StockDTO[] resultContent = JsonConvert.DeserializeObject<StockDTO[]>(contentString);
+
+                    lstBox.Items.AddRange(resultContent);
+                }
+                else
+                {
+                    MessageBox.Show("DepoAPI ile ilgili bir sorun oluştu");
+                }
+            }
+
+
+
+            //Diğer yolu bu, bu yolun tek sıkıntısı API kapalıysa yada çalışmıyorsa bu proje patlıyor.
+
+            //using (HttpClient client = new HttpClient())
+            //{
+            //    client.BaseAddress = new Uri("https://localhost:44339/api/");
+            //    HttpResponseMessage gettask = await client.GetAsync("home/getstock");
+            //    gettask.EnsureSuccessStatusCode();
+
+            //    string contentstring = await gettask.Content.ReadAsStringAsync();
+
+            //    StockDTO[] resultcontent = JsonConvert.DeserializeObject<StockDTO[]>(contentstring);
+
+            //    lstBox.Items.AddRange(resultcontent);
+            //}
+        }
+    }
+}
+
